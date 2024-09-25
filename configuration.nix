e2418903d508f3d9031820cd4bcf274247d4992e
Markéta Bartáková <bartakova.ma@gmail.com>
2024-09-25 18:43:46 +0200

    Initial commit

--- configuration.nix
+++ configuration.nix
@@ -8,96 +8,143 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+       <catppuccin/modules/nixos>
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking = {
+    hostName = "zena-pc"; # Define your hostname.
+    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+    # Open ports in the firewall.
+    firewall = {
+      allowedTCPPorts = [80 420 57621];
+      allowedUDPPorts = [ 5353 ];
+    };
+
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  time.timeZone = "Europe/Prague";
 
 
-  
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services = {
+    openssh.enable = true;
+    displayManager.sddm = {
+	package = pkgs.kdePackages.sddm;
+	enable = true;
+    };
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+    xserver = {
+      enable = true;
+      desktopManager.plasma5.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+      xkb.layout = "cz"; 
+      xkb.options = "eurosign:e,caps:escape";
+    };
+  };
 
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+  users.users.loading = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "nixos" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3Nz....6OWM= user" # content of authorized_keys file
+    ];
+    packages = with pkgs; [
+      firefox
+      tree
+      kitty
+      steam
+      discord
+      (discord.override {
+        withVencord = true;
+      })
+      lutris
+
+      catppuccin
+      catppuccin-kde
+      libreoffice
+
+      jetbrains.clion
+      jetbrains.rider
+      jetbrains.idea-ultimate
+   
+      spotify
+      haskellPackages.github
+ ];
+    shell = pkgs.zsh;
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    neovim 
+    wget
+    curl
+    git
+    zoxide
+    zsh
+    pfetch-rs
+    sselp
+    wacomtablet
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    gcc
+    rustc
+    cargo
+    python3
+    acpi
+    gnomeExtensions.upower-battery
+    pavucontrol
+    alsa-tools
+    blueman 
+    spotify
+    gnomeExtensions.duckduckgo-search-provider
+ ];
 
-  # List services that you want to enable:
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+   mtr.enable = true;
+   gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+   };
+   zsh.enable = true;
+  };
+  catppuccin.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
@@ -117,6 +164,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
-
